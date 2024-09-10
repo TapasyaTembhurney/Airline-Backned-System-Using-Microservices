f1d6bd15aaac2cd719e88653921790aeb441d257
Your name <tapasyatembhurney@gmail.com>
2024-09-10 13:17:26 +0530

    city repository created

--- Flights-And_Search/src/repository/city-repository.js
+++ Flights-And_Search/src/repository/city-repository.js
@@ -1,27 +1,53 @@
-const {City} = require("../models/index");
+const { City } = require("../models/index");
 
 class CityRepository {
-    async createCity ({name}) {
+    async createCity({ name }) { //{name: "New Delhi"}
         try {
-            const city = await City.create({name}) ;
+            const city = await City.create({ name });
             return City;
-        } catch(error){
-            console.error('Error creating city :' , error);
-            throw(error);
+        } catch (error) {
+            console.error('Something went wrong:', error);
+            throw (error);
         }
     }
 
 
     async deleteCity(cityId) {
         try {
-            await city.destroy({
+            await City.destroy({
                 where: {
                     id: cityId
                 }
             })
+            return true;
         } catch (error) {
-            console.error('Error deleting city:', error);
-            throw {error}; 
+            console.error('Something went wrong :', error);
+            throw { error };
+        }
+    }
+    async updateCity(cityId, data) {
+        try {
+            const city = await City.update(data, {
+                where: {
+                    id: cityId
+                }
+            })
+            return City;
+        }
+        catch (error) {
+            console.error("Something went wrong:", error);
+            throw{error};
+        }
+    }
+
+    async getCity(cityId) {
+        try {
+            const city = await City.findByPk(cityId);
+            return city;
+        }
+        catch (error) {
+            console.error("Something went wrong:", error);
+            throw {error};
         }
     }
 }
