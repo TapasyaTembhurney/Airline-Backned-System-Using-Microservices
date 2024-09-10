20ffc9d1936a62a5947e644ead0098cc81a47566
Your name <tapasyatembhurney@gmail.com>
2024-09-10 14:24:35 +0530

    added filters to get all cities

--- Flights-And_Search/src/repository/city-repository.js
+++ Flights-And_Search/src/repository/city-repository.js
@@ -1,3 +1,4 @@
+const { Op } = require("sequelize");
 const { City } = require("../models/index");
 
 class CityRepository {
@@ -27,7 +28,7 @@ class CityRepository {
     }
     async updateCity(cityId, data) {
         try {
-             // The below approch also works but will not return updated object
+            // The below approch also works but will not return updated object
             // if we are using pg then returning true can be used, else not.
             // const City = await city.update(data, {
             //     where: {
@@ -35,14 +36,14 @@ class CityRepository {
             //     }
             // });
             // for getting updated data in mysql we use the below approach.
-            const City = await city.findByPk(cityId);
+            const city = await City.findByPk(cityId);
             City.name = data.name;
-            await City.save(); 
-            return City;
+            await city.save();
+            return city;
         }
         catch (error) {
             console.error("Something went wrong at repository layer", error);
-            throw{error};
+            throw { error };
         }
     }
 
@@ -53,17 +54,28 @@ class CityRepository {
         }
         catch (error) {
             console.error("Something went wrong at repository layer", error);
-            throw {error};
+            throw { error };
         }
     }
 
-    async getAllCities() {
+    async getAllCities(filter) {  //filter can be empty also .
         try {
-            const cities = await city.findAll();
+
+            if (filter.name) {
+                const cities = await City.findAll({
+                    where: {
+                        name: {
+                            [Op.startsWith]: filter.name
+                        }
+                    }
+                })
+                return cities;
+            }
+            const cities = await City.findAll();
             return cities;
-        }catch (error){
-            console.log("Something went wrong at repository layer" , error) ;
-            throw(error);
+        } catch (error) {
+            console.log("Something went wrong at repository layer", error);
+            throw (error);
         }
     }
 }
