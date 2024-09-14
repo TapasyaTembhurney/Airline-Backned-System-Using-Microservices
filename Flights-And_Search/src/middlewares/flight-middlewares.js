57bc6429b7839c1ef0df84668d78ffa15d635fe1
Your name <tapasyatembhurney@gmail.com>
2024-09-14 18:46:14 +0530

    added error codes enums

--- Flights-And_Search/src/middlewares/flight-middlewares.js
+++ Flights-And_Search/src/middlewares/flight-middlewares.js
@@ -1,3 +1,5 @@
+const { ClientErrorsCodes } = require("../utils/error-codes");
+
 const validateCreateFlight = (req, res, next) => {
     if( !req.body.flightNumber ||
         !req.body.airplaneId ||
@@ -8,7 +10,7 @@ const validateCreateFlight = (req, res, next) => {
         !req.body.price 
     ) {
         // if any of the body params is missing we come inside the if
-        return res.status(400).json({
+        return res.status(ClientErrorsCodes.BAD_REQUEST).json({
             data: {},
             success: false,
             message: 'Invalid request body for create flight',
