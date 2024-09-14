a65ea539ce23c7f3864245d8cd85307128b4224e
Your name <tapasyatembhurney@gmail.com>
2024-09-14 18:41:40 +0530

    Added flight create request middleware

--- Flights-And_Search/src/routes/v1/index.js
+++ Flights-And_Search/src/routes/v1/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const CityController = require("../../controllers/city-controller");
-const FlightController = require("../../controller/flight-controller");
+const { CityController, FlightController, AirportController } = require("../../controllers/index");
+const { FlightMiddlewares } = require("../../middlewares/index");
 
 const router = express.Router();
 
@@ -9,7 +9,9 @@ router.delete('/city/:id', CityController.destroy);
 router.get('/city/:id', CityController.get);
 router.get('/city', CityController.getAll);
 router.patch('/city/:id', CityController.update);
-router.post('/flights', FlightController.create);
+
+
+router.post('/flights', FlightMiddlewares.validateCreateFlight, FlightController.create);
 router.get('/flights', FlightController.getAll);
 router.post("/airports", AirportController.create);
 
