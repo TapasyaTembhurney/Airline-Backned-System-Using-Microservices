85fb83f9acc10d121afeae8aaf5668204ab50b3e
Your name <tapasyatembhurney@gmail.com>
2024-09-29 12:08:27 +0530

    added error files

---
+++ BookingService/src/utils/errors/service-error.js
@@ -0,0 +1,16 @@
+const { statusCode } = require("http-status-codes");
+
+class ServiceError extends Error {
+    constructor(
+        message = 'Something went wrong',
+        explanation,
+        statusCode = StatusCodes.INTERNAL_SERVER_ERROR
+    ) {
+        super();
+        this.name = 'ServiceError';
+        this.message = message;
+        this.explanation = explanation;
+        this.statusCode = statusCode;
+    }
+}
+module.exports = ServiceError;
